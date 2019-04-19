ff44dbf0a9946877a4edca2d3314e569e10cabfc
ChikaKanu <chika.kanu@procompletions.com>
2019-04-19 14:39:12 +0100

    category seed data create

--- sports_news_full_stack_app/SportsNewsBE/src/main/java/com/codeclan/example/SportsNews/components/DataLoader.java
+++ sports_news_full_stack_app/SportsNewsBE/src/main/java/com/codeclan/example/SportsNews/components/DataLoader.java
@@ -1,4 +1,28 @@
 package com.codeclan.example.SportsNews.components;
 
-public class DataLoader {
+import com.codeclan.example.SportsNews.models.Category;
+import com.codeclan.example.SportsNews.repositories.Categories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    public DataLoader() {}
+
+    public void run(ApplicationArguments args) {
+
+
+        Category football = new Category("Football");
+        categoryRepository.save(football);
+
+        Category baseball = new Category("Baseball");
+        categoryRepository.save(baseball);
+
+    }
 }
--- sports_news_full_stack_app/SportsNewsBE/src/test/java/com/codeclan/example/SportsNews/SportsNewsApplicationTests.java
+++ sports_news_full_stack_app/SportsNewsBE/src/test/java/com/codeclan/example/SportsNews/SportsNewsApplicationTests.java
@@ -1,16 +1,24 @@
 package com.codeclan.example.SportsNews;
 
+import com.codeclan.example.SportsNews.repositories.Categories.CategoryRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SportsNewsApplicationTests {
 
+	@Autowired
+	CategoryRepository categoryRepository;
+
 	@Test
 	public void contextLoads() {
 	}
+
+
 
 }
