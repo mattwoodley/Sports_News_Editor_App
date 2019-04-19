006d825f9d37007cc0834fd6582a2684420724c4
EmilZacharczuk <emil.zacharczuk@yahoo.pl>
2019-04-19 17:50:01 +0100

    article tested

--- sports_news_full_stack_app/SportsNewsBE/src/main/java/com/codeclan/example/SportsNews/components/DataLoader.java
+++ sports_news_full_stack_app/SportsNewsBE/src/main/java/com/codeclan/example/SportsNews/components/DataLoader.java
@@ -1,13 +1,20 @@
 package com.codeclan.example.SportsNews.components;
 
+import com.codeclan.example.SportsNews.models.Article;
 import com.codeclan.example.SportsNews.models.Category;
 import com.codeclan.example.SportsNews.models.Journalist;
+import com.codeclan.example.SportsNews.repositories.Articles.ArticleRepository;
 import com.codeclan.example.SportsNews.repositories.Categories.CategoryRepository;
 import com.codeclan.example.SportsNews.repositories.Journalists.JournalistRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Component
 public class DataLoader implements ApplicationRunner {
@@ -18,10 +25,20 @@ public class DataLoader implements ApplicationRunner {
     @Autowired
     JournalistRepository journalistRepository;
 
+    @Autowired
+    ArticleRepository articleRepository;
+
     public DataLoader() {}
 
     public void run(ApplicationArguments args) {
-
+        DateFormat sfd = new SimpleDateFormat("dd-MM-yy");
+        String newDate= "11-02-2019";
+        Date date = null;
+        try {
+            date = sfd.parse(newDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
 
         Category football = new Category("Football");
         categoryRepository.save(football);
@@ -34,5 +51,8 @@ public class DataLoader implements ApplicationRunner {
 
         Journalist journalist2 = new Journalist("Gordon", "Doe", "url");
         journalistRepository.save(journalist2);
+
+        Article articleFootball = new Article("Football", "About football", "Long story about football", date, "url", 7);
+        articleRepository.save(articleFootball);
     }
 }
