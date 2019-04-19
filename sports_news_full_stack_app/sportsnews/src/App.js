a2c7a1b1a80484a3ead079e524c035b5772c4a39
ghr85 <ghrenfrew@gmail.com>
2019-04-19 17:58:46 +0100

    updated app.js based on initial wireframes

--- sports_news_full_stack_app/sportsnews/src/App.js
+++ sports_news_full_stack_app/sportsnews/src/App.js
@@ -1,28 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
+import MainContainer from './containers/MainContainer';
+import NavBar from './containers/NavBar';
+import Header from './containers/Header';
+import ArticleListContainer from './containers/articles/ArticleListContainer'
+import SingleArticleContainer from './containers/articles/SingleArticleContainer';
+import ArticleFormContainer from './containers/articles/ArticleFormContainer';
+import ArticleEditFormContainer from './containers/articles/ArticleEditFormContainer';
+import JournalistListContainer from './containers/articles/JournalistListContainer'
+import SingleJournalistContainer from './containers/articles/SingleJournalistContainer';
+import JournalistFormContainer from './containers/articles/JournalistFormContainer';
+import JournalistEditFormContainer from './containers/articles/JournalistEditFormContainer';
+import CategoriesListContainer from './containers/categories/CategoriesListContainer'
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+      <Router >
+        <React.Fragment>
+          <Header/>
+          <NavBar/>
+          <Switch>
+            <Route exact path = '/' component={MainContainer}/>
+            <Route exact path = '/articles' component={ArticleListContainer}/>
+            <Route exact path = '/articles/new' component={ArticleFormContainer}/>
+            <Route exact path="/articles/edit/:id" render = {(props) =>{
+              const id = props.match.params.id;
+              return <ArticleEditFormContainer id = {id} />
+            }}
+          />
+          <Route exact path="/articles/:id" render = {(props) =>{
+            const id = props.match.params.id;
+            return <SingleArticleContainer id = {id} />
+          }}
+        />
+        <Route exact path = '/articles' component={JournalistListContainer}/>
+        <Route exact path = '/articles/new' component={JournalistFormContainer}/>
+        <Route exact path="/articles/edit/:id" render = {(props) =>{
+          const id = props.match.params.id;
+          return <JournalistEditFormContainer id = {id} />
+        }}
+      />
+      <Route exact path="/articles/:id" render = {(props) =>{
+        const id = props.match.params.id;
+        return <SingleJournalistContainer id = {id} />
+      }}
+      <Route exact path="/categories/:id" render = {(props) =>{
+        const id = props.match.params.id;
+        return <CategoriesListContainer id = {id} />
+      }}
+    />
+  </Switch>
+</React.Fragment>
+</Router>
+);
+}
 }
 
 export default App;
