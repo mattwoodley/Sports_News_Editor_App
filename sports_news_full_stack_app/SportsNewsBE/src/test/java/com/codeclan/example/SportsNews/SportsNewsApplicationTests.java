3ae0a636eb708b0e145852bb2cb034d96da91a7e
ChikaKanu <chika.kanu@procompletions.com>
2019-04-19 14:46:10 +0100

    getName test

--- sports_news_full_stack_app/SportsNewsBE/src/test/java/com/codeclan/example/SportsNews/SportsNewsApplicationTests.java
+++ sports_news_full_stack_app/SportsNewsBE/src/test/java/com/codeclan/example/SportsNews/SportsNewsApplicationTests.java
@@ -1,11 +1,14 @@
 package com.codeclan.example.SportsNews;
 
+import com.codeclan.example.SportsNews.models.Category;
 import com.codeclan.example.SportsNews.repositories.Categories.CategoryRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
 
 
 @RunWith(SpringRunner.class)
@@ -19,6 +22,9 @@ public class SportsNewsApplicationTests {
 	public void contextLoads() {
 	}
 
-
-
+	@Test
+	public void getName() {
+		Category football = new Category("Football");
+		assertEquals("Football", football.getName());
+	}
 }
