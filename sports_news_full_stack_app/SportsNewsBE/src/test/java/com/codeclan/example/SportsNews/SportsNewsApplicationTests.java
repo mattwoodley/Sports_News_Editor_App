a6fbd81b4bed77a913a9effe652c0158b9ea7490
ChikaKanu <chika.kanu@procompletions.com>
2019-04-20 05:51:14 +0100

    basic tests complete

--- sports_news_full_stack_app/SportsNewsBE/src/test/java/com/codeclan/example/SportsNews/SportsNewsApplicationTests.java
+++ sports_news_full_stack_app/SportsNewsBE/src/test/java/com/codeclan/example/SportsNews/SportsNewsApplicationTests.java
@@ -93,4 +93,47 @@ public class SportsNewsApplicationTests {
 		football.addArticle(footballArticle);
 		assertEquals(1, football.getArticles().size());
 	}
+
+	@Test
+	public void canAddArticleToCategoryAndJournalistRepoThenSave() {
+		DateFormat sfd = new SimpleDateFormat("dd-MM-yy");
+		String newDate= "11-02-2019";
+		Date date = null;
+		try {
+			date = sfd.parse(newDate);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		Category football = new Category("Football");
+		categoryRepository.save(football);
+
+		Category baseball = new Category("Baseball");
+		categoryRepository.save(baseball);
+
+		Journalist journalist1 = new Journalist("John", "Burn", "url");
+		journalistRepository.save(journalist1);
+
+		Journalist journalist2 = new Journalist("Gordon", "Doe", "url");
+		journalistRepository.save(journalist2);
+
+		Article articleFootball1 = new Article("Football", "About football", "Long story about football", date, "url", 7, football, journalist2);
+		articleRepository.save(articleFootball1);
+
+		Article articleFootball2 = new Article("Football", "About football", "The winner", date, "url", 5, football, journalist1);
+		articleRepository.save(articleFootball2);
+
+		Article articleBaseball1 = new Article("Baseball", "About baseball", "Great team of great people", date, "url", 2, baseball, journalist1);
+		articleRepository.save(articleBaseball1);
+
+		football.addArticle(articleFootball1);
+		football.addArticle(articleFootball2);
+		categoryRepository.save(football);
+
+		journalist1.addArticle(articleFootball2);
+		journalist1.addArticle(articleBaseball1);
+		journalistRepository.save(journalist1);
+		journalist2.addArticle(articleFootball1);
+		journalistRepository.save(journalist2);
+	}
 }
