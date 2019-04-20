ec04ea282f2fd2d73837d0fc3ccb392fe36f42b0
ghr85 <ghrenfrew@gmail.com>
2019-04-20 09:52:41 +0100

    updated articlelistcontainer

--- sports_news_full_stack_app/sportsnews/src/containers/articles/ArticleListContainer.js
+++ sports_news_full_stack_app/sportsnews/src/containers/articles/ArticleListContainer.js
@@ -2,8 +2,27 @@ import React, {Component} from 'react';
 import ArticleList from '../../components/articles/ArticleList.js';
 import Request from '../../helpers/request.js';
 
-class ArticleListContainer extends Component {
 
+class ArticleListContainer extends Component {
+  constructor(props){
+    super(props);
+    this.state = {articles: []}
+  }
+
+//link in to back end api to fetch all articles store in state, TODO sort conditions TODO investigate embedded?
+  componentDidMount(){
+    let request = new Request()
+    request.get('/api/articles').then((data) => {
+      this.setState({articles: data})
+    })
+  }
+
+//render the main article list (which itself renders individual microarticles)
+  render(){
+    return (
+     <ArticleList articles = {this.state.articles} />
+    )
+  }
 }
 
 export default ArticleListContainer;
