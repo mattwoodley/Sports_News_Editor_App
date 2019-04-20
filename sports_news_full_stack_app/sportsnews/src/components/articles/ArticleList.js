c91f67e28efdd359771e757058d75d8a427d91f2
ghr85 <ghrenfrew@gmail.com>
2019-04-20 10:06:00 +0100

    updated articlelist component

--- sports_news_full_stack_app/sportsnews/src/components/articles/ArticleList.js
+++ sports_news_full_stack_app/sportsnews/src/components/articles/ArticleList.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import MicroArticle from './MicroArticle.js';
 
-
+//Article container renders article list - article list responsible for rendering  an ul of microarticle elements - list format to be used for CSS styling.
 const ArticleList = (props) => {
+  const articles = props.articles.map((article) => {
+		 	return (
+				<li key={article.id} className="microarticle-li">
+					<div className = "micro-article">
+				<MicroArticle headline={article.headline} summary={article.summary} image={article.image} dateCreated={article.dateCreated} />
+			</div>
+			</li>
+		)
+		})
 
+	return (
+		<ul className="microarticle-ul">
+	    {articles}
+	  </ul>
+
+	)
 }
 
 export default ArticleList;
