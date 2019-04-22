b729f7c7f642b950b37885a4de7b07fd0327a609
ghr85 <ghrenfrew@gmail.com>
2019-04-22 08:02:21 +0100

    bugfix: corrected render function in microjournalist

--- sports_news_full_stack_app/sportsnews/src/components/journalists/MicroJournalist.js
+++ sports_news_full_stack_app/sportsnews/src/components/journalists/MicroJournalist.js
@@ -5,13 +5,14 @@ import {Link} from 'react-router-dom';
 //note the use of fragment to avoid creating a dom node - each microarticle is contained within the parent li or div element - this will allow for different layouts of the same component
 
 const MicroJournalist= (props) => {
-  <React.Fragment>
+  return(<React.Fragment>
     <Link to = {"/journalists/" + props.id} className="microjournalist-img-link">
           <img src={props.image}/>
         </Link>
         <p className='microjournalistFirstName'>{props.firstName}</p>
         <p className='microjournalistSurname'>{props.surname}</p>
   </React.Fragment>
+)
 }
 
 export default MicroJournalist;
