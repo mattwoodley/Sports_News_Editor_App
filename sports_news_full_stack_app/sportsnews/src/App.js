f57bf29bdcee0388935276818531d88a4f5ba418
Matthew Woodley <Woodlehh@gmail.com>
2019-04-22 10:26:02 +0100

    comment syntax fixes

--- sports_news_full_stack_app/sportsnews/src/App.js
+++ sports_news_full_stack_app/sportsnews/src/App.js
@@ -26,29 +26,29 @@ class App extends Component {
             <Route exact path = '/' component={MainContainer}/>
             <Route exact path = '/articles' component={ArticleListContainer}/>
             <Route exact path = '/articles/new' component={ArticleCreateFormContainer}/>
-            <Route exact path="/articles/edit/:id" render = {(props) =>{
+            <Route exact path = '/articles/edit/:id' render = {(props) =>{
               const id = props.match.params.id;
               return <ArticleEditFormContainer id = {id} />
             }}
             />
-            <Route exact path="/articles/:id" render = {(props) =>{
+            <Route exact path = '/articles/:id' render = {(props) =>{
               const id = props.match.params.id;
               return <SingleArticleContainer id = {id} />
             }}
             />
             <Route exact path = '/journalists' component={JournalistListContainer}/>
             <Route exact path = '/journalists/new' component={JournalistCreateFormContainer}/>
-            <Route exact path="/journalists/edit/:id" render = {(props) =>{
+            <Route exact path = '/journalists/edit/:id' render = {(props) =>{
               const id = props.match.params.id;
               return <JournalistEditFormContainer id = {id} />
             }}
             />
-            <Route exact path="/journalists/:id" render = {(props) =>{
+            <Route exact path = '/journalists/:id' render = {(props) =>{
               const id = props.match.params.id;
               return <SingleJournalistContainer id = {id} />
             }}
             />
-            <Route exact path="/categories/:id" render = {(props) =>{
+            <Route exact path = '/categories/:id' render = {(props) =>{
               const id = props.match.params.id;
               return <CategoriesListContainer id = {id} />
             }}
