9d11a87b8bb8075869cf23dda890f964f67641ed
ghr85 <ghrenfrew@gmail.com>
2019-04-22 07:47:13 +0100

    updated journalistListcontainer.js

--- sports_news_full_stack_app/sportsnews/src/containers/journalists/JournalistListContainer.js
+++ sports_news_full_stack_app/sportsnews/src/containers/journalists/JournalistListContainer.js
@@ -3,7 +3,26 @@ import JournalistList from '../../components/journalists/JournalistList.js';
 import Request from '../../helpers/request.js';
 
 class JournalistListContainer extends Component {
+  constructor(props){
+    super(props);
+    this.state = {journalists: []}
+  }
 
+//link in to back end api to fetch all journalists store in state
+componentDidMount(){
+    let request = new Request();
+    request.get('/api/journalists').then((data) => {
+      this.setState({journalists: data._embedded.journalists})
+    })
+  }
+
+
+//render the main journalist  list (which itself renders individual microjournalists)
+  render(){
+    return (
+     <JournalistList journalist = {this.state.journalists} />
+    )
+  }
 }
 
 export default JournalistListContainer;
