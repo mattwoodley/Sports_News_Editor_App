ad0527a25ec1d2dbbe708e4ec0614952623159ad
ghr85 <ghrenfrew@gmail.com>
2019-04-22 14:24:03 +0100

    updated createjournalistcontainer.js

--- sports_news_full_stack_app/sportsnews/src/containers/journalists/JournalistCreateFormContainer.js
+++ sports_news_full_stack_app/sportsnews/src/containers/journalists/JournalistCreateFormContainer.js
@@ -4,6 +4,28 @@ import Request from '../../helpers/request.js';
 
 class JournalistCreateFormContainer extends Component {
 
+  constructor(props){
+    super(props);
+    this.handleJournalistPost = this.handleJournalistPost.bind(this);
+  }
+
+  //make a post request to api, giving it the article in JSON
+  handleArticlePost(journalists){
+    const request = new Request();
+    request.post('/api/journalists', journalist).then(() => {
+      window.location = '/journalists'
+    });
+  }
+
+  //render an ArticleCreateForm, feed it available journalists and categories as well as functionality to actually post to be rendered in button
+  render(){
+
+    return (
+      <JournalistCreateForm
+        handleArticlePost={this.handleArticlePost}
+      />
+    )
+  }
 }
 
 export default JournalistCreateFormContainer;
